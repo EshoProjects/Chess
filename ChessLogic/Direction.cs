60504c58ee006e6b291243894a19a1aa338ca939
eshog <eshog@BOOK-USM9MV6IU2>
2024-12-23 10:44:47 +0100

    Paus fortsättning med Directions igen

--- ChessLogic/Direction.cs
+++ ChessLogic/Direction.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     internal class Direction
     {
+        public int RowDelta {  get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction a, Direction b)
+        {
+            return new Direction(a.RowDelta + b.RowDelta, a.ColumnDelta + b.ColumnDelta);
+           
+        }
+
+        public static Direction operator *(int scalar, Direction dir)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+        }
+
+        
     }
 }
