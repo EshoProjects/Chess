006a2e982b1cefbfdecb961dc5f30502a514b07f
eshog <eshog@BOOK-USM9MV6IU2>
2024-12-23 08:17:55 +0100

    Add project files.

---
+++ ChessLogic/Position.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChessLogic
+{
+    public enum SquareColor
+    {
+        White,
+        Black
+    }
+
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public SquareColor GetSquareColor()
+        {
+            // Bestämmer färg på rutan baserat på rad och kolumn
+            return (Row + Column) % 2 == 0 ? SquareColor.White : SquareColor.Black;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+    }
+}
